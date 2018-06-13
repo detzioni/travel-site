9c18723ee63e36508f224f4e6fd83aefd96790f4
Doron Etzioni <doron.etzioni@uPundit.com>
2018-06-13 17:40:59 -0400

    Large display menu enhanchments and smooth scrooling

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,7 +1,9 @@
 import $ from 'jquery';
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
+import StickyHeader from './modules/StickyHeader';
 
+new StickyHeader();
 new MobileMenu();
 new RevealOnScroll($('.feature-item'), "85%");
 new RevealOnScroll($('.testimonials'), "65%");
