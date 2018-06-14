ecacf287320977660caaa9f3a42441dd1f6c4706
Doron Etzioni <doron.etzioni@uPundit.com>
2018-06-13 20:20:42 -0400

    Completed building a Modal

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -2,8 +2,11 @@ import $ from 'jquery';
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import StickyHeader from './modules/StickyHeader';
+import Modal from './modules/Modal';
 
 new StickyHeader();
 new MobileMenu();
 new RevealOnScroll($('.feature-item'), "85%");
 new RevealOnScroll($('.testimonials'), "65%");
+
+var modal = new Modal();
